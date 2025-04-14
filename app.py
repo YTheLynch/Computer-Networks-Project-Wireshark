5d6a4cec3cf9319bacf13000084601ce1923d935
Yuganshu Kumar <69767140+YTheLynch@users.noreply.github.com>
2025-04-14 12:02:34 +0530

    remove unused import send_from_directory

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+from flask import Flask, request, render_template, redirect, url_for
 from scapy.all import rdpcap
 from collections import Counter
 import matplotlib
